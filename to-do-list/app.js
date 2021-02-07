74dc1eb28dc94834cf66319a9c30cc4d51675de0
Nikita Fedarenchyk <fnikita01@mail.ru>
2021-02-07 16:29:16 +0300

    feat: add ability to filter elements of the list

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,19 +1,18 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const searchButton = document.querySelector(".search-button");
 
-todoButton.addEventListener('click',addTodo);
+todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 
+searchButton.addEventListener('click', filterTodo);
 
-
-
-
-function addTodo(event){
+function addTodo(event) {
     event.preventDefault();
     console.log("click");
 
-    const  todoDiv = document.createElement('div');
+    const todoDiv = document.createElement('div');
     todoDiv.classList.add("todo");
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
@@ -35,21 +34,57 @@ function addTodo(event){
     todoInput.value = "";
 }
 
-function deleteCheck(e){
+function deleteCheck(e) {
     console.log(e.target);
-    const  item = e.target;
+    const item = e.target;
 
-    if (item.classList[0] === 'trash-btn'){
-        const  todo = item.parentElement;
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
         todo.classList.add("fall");
-        todo.addEventListener("transitionend", function (){
+        todo.addEventListener("transitionend", function () {
             todo.remove();
         });
     }
 
-    if (item.classList[0] === 'complete-btn'){
+    if (item.classList[0] === 'complete-btn') {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(e) {
+    e.preventDefault();
+
+    let sel = document.getElementById('selecter');
+    let val = sel.options[sel.selectedIndex].value;
+
+    const todos = todoList.childNodes;
+    for (let i = 1; i < todos.length; i++) {
+        let todo = todos[i];
+        switch (val) {
+            case "all":
+                todo.style.display = 'flex';
+                console.log("all");
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = 'flex';
+                    console.log("com fl");
+                } else {
+                    todo.style.display = 'none';
+                    console.log("com none");
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = 'flex';
+                    console.log("un fl");
+                } else {
+                    todo.style.display = 'none';
+                    console.log("un none");
+                }
+                break;
+        }
     }
 }
 
